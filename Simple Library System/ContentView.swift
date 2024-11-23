d7ebe1caf50c0147a53513e74ce95d28309f9fcb
michellealvera <malveralolang@student.ciputra.ac.id>
2024-11-23 23:25:44 +0700

    Initial Commit

---
+++ Simple Library System/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simple Library System
+//
+//  Created by Michelle Alvera Lolang on 23/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simple Library System/Simple_Library_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Library_SystemApp.swift
+//  Simple Library System
+//
+//  Created by Michelle Alvera Lolang on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Library_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
